f32e4ab76cf0ea96b6eb22e7a15c31715b0fdee6
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-07-24 13:26:05 -0500

    Update inject-langs.js

--- lenguapedia/default/inject-langs.js
+++ lenguapedia/default/inject-langs.js
@@ -16,27 +16,8 @@ void async function InjectLangs(){
       } catch (e) { continue; }
     }
    
-    /*let srcs = document.querySelectorAll('[src]:not([src*="xxlangsxx"])');
-    const srcs_length = srcs.length;
-    for (let i = 0; i < srcs_length; i++) {
-      try {
 
-        srcs[i].setAttribute('src', srcs[i].src+'?xxlangsxx'+xlangs+'xxlangsxx');
-
-      } catch (e) { continue; }
-    }*/
    
-
-    let dataSrcs = document.querySelectorAll('[data-src]:not([data-src*="langs="])');
-    const dataSrcs_length = dataSrcs.length;
-    for (let i = 0; i < dataSrcs_length; i++) {
-      try {
-        let prefix='?';
-        if(dataSrcs[i].getAttribute('data-src').includes('?')){prefix='&';}
-        dataSrcs[i].setAttribute('data-src', dataSrcs[i].getAttribute('data-src')+prefix'langs='+xlangs);
-
-      } catch (e) { continue; }
-    }
 
 
   }, 100);
