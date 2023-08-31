7124f2cb47b99e281231239c4af4f10028e04b1a
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-08-31 11:10:42 -0500

    Update inject-langs.js

--- lenguapedia/default/inject-langs.js
+++ lenguapedia/default/inject-langs.js
@@ -6,22 +6,34 @@ void async function InjectLangs(){
    let xlangs = document.querySelector('lenguapedia').getAttribute('xlangs');
    
     let hrefs = document.querySelectorAll('[href]:not([href*="langs="])');
-    let uselang='';
-    if(window.location.href.includes('uselang')){
-     uselang='&uselang='+window.location.href.split('uselang=')[1].split('&')[0].split('#')[0];
-    }
-    const hrefs_length = hrefs.length;
+
+    let hrefs_length = hrefs.length;
     for (let i = 0; i < hrefs_length; i++) {
       try {
         let prefix='?';
         if(hrefs[i].getAttribute('href').includes('?')){prefix='&';}
-        hrefs[i].setAttribute('href', hrefs[i].href+prefix+'langs='+xlangs+uselang);
+        hrefs[i].setAttribute('href', hrefs[i].href+prefix+'langs='+xlangs);
 
       } catch (e) { continue; }
     }
    
 
+
    
+    hrefs = document.querySelectorAll('[href]:not([href*="uselang="])');
+    let uselang='uselang=';
+    if(window.location.href.includes('uselang=')){
+     uselang='uselang='+window.location.href.split('uselang=')[1].split('&')[0].split('#')[0];
+    }
+   hrefs_length = hrefs.length;
+    for (let i = 0; i < hrefs_length; i++) {
+      try {
+        let prefix='?';
+        if(hrefs[i].getAttribute('href').includes('?')){prefix='&';}
+        hrefs[i].setAttribute('href', hrefs[i].href+prefix+uselang);
+
+      } catch (e) { continue; }
+    }
 
 
   }, 100);
