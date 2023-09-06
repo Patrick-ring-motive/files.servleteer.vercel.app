973319d9d437566f625deb5019aff86dd60dbe35
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-09-06 15:31:13 -0500

    Update fandom-block.js

--- fandom/fandom-block.js
+++ fandom/fandom-block.js
@@ -206,5 +206,5 @@ async function jsonpFetch(url){
 
 
       
-textNodesUnder(document);
-setTimeout(function(){textNodesUnder(document);},100);
+textNodesUnder(document.body||document);
+setTimeout(function(){textNodesUnder(document.body);},100);
