5fa784a15b55b820c7efd0a5761ecf127c1be43b
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-09-09 20:52:14 -0500

    Update decode-fix.js

--- fandom/decode-fix.js
+++ fandom/decode-fix.js
@@ -11,7 +11,7 @@ if(!globalThis.decodeTable){
 globalThis.decodeTable=[];
 
 
-for(let i=0;i<10000;i++){
+for(let i=0;i<1000;i++){
 let char =String.fromCharCode(i);
 const encoder = new TextEncoder();
 const view = encoder.encode(char);
