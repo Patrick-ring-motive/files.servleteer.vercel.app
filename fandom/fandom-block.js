98401a90328007755b22fa8e493ffcfd74d086f2
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-09-11 09:58:13 -0500

    Update fandom-block.js

--- fandom/fandom-block.js
+++ fandom/fandom-block.js
@@ -118,7 +118,16 @@ function checkReferer(){
 let referer=document.querySelector('http-header[key="referer"]');
 if(referer){
   let refererHost=referer.getAttribute('value').split('/')[2];
-  if(refererHost&&(refererHost!=window.location.host)&&refererHost.includes('lenguapedia.org')){
+  if(refererHost
+     &&(refererHost
+        .replace('-','')
+        .replace('_','')
+        .replace('.','')
+        !=window.location.host
+        .replace('-','')
+        .replace('_','')
+        .replace('.',''))
+     &&refererHost.includes('lenguapedia.org')){
 console.log(refererHost, window.location.host);
     let window_location=window.location.href.split('/');
     window_location[2]=refererHost;
