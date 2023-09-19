5f5aa86dacb69df3730c6b516d2f8bc37c207d46
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-09-19 10:00:46 -0500

    Update check-referer.js

--- lenguapedia/check-referer.js
+++ lenguapedia/check-referer.js
@@ -3,7 +3,7 @@ void function checkReferer(){
 let referer=document.querySelector('http-header[key="referer"]');
 if(referer){
   let refererHost=referer.getAttribute('value').split('/')[2];
-  if(refererHost&&(refererHost!=window.location.host)&&refererHost.includes('lenguapedia.org')){
+  if(refererHost&&(refererHost!=window.location.host)&&refererHost.includes('lenguapedia.org')&&(!window.location.host.includes('lenguapedia.org'))){
 console.log(refererHost, window.location.host);
     let window_location=window.location.href.split('/');
     window_location[2]=refererHost;
