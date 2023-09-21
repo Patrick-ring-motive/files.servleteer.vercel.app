ddff1384f21713147bbbd89f5df00dc9c0e5d02f
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-09-21 09:28:23 -0500

    Update check-referer.js

--- lenguapedia/check-referer.js
+++ lenguapedia/check-referer.js
@@ -11,4 +11,18 @@ console.log(refererHost, window.location.host);
     referer.remove();
   }
 }  
+
+let lowcation=window.location.href.toLowerCase();
+
+if(lowcation.includes('referer='){
+  let refererHost=lowcation.split('referer=')[1].split('&')[0].split('#')[0];
+  if(refererHost&&(refererHost!=window.location.host)&&refererHost.includes('lenguapedia.org')&&(!window.location.host.includes('lenguapedia.org'))){
+console.log(refererHost, window.location.host);
+    let window_location=window.location.href.split('/');
+    window_location[2]=refererHost;
+    window.location.href=window_location.join('/');
+    referer.remove();
+  }
+}  
+  
 }();
